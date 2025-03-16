ca14d9d30bd7999270913a0cfaa9c73c9389b69f
Yan <134351027+Yanmaiscedo@users.noreply.github.com>
2025-03-16 19:12:54 -0300

    Feat: Variables

--- SnakeGame.c
+++ SnakeGame.c
@@ -3,6 +3,17 @@
 #include <conio.h>
 #include <time.h>
 
+#define WIDTH 40
+#define HEIGHT 27
+
+int gameOver;
+int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
+
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+enum eDirection dir;
+
 int main()
 {
 

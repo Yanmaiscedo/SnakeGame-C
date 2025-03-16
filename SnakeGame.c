ef5825860f4ccd3f7bb786058b1b8506ad45ba3f
Yan <134351027+Yanmaiscedo@users.noreply.github.com>
2025-03-16 19:18:59 -0300

    Feat: Imput function

--- SnakeGame.c
+++ SnakeGame.c
@@ -56,6 +56,18 @@ void Draw() {
     printf("\nScore: %d\n", score);
 }
 
+void Input() {
+    if (_kbhit()) {
+        switch (_getch()) {
+            case 'a': dir = LEFT; break;
+            case 'd': dir = RIGHT; break;
+            case 'w': dir = UP; break;
+            case 's': dir = DOWN; break;
+            case 'x': gameOver = 1; break;
+        }
+    }
+}
+
 int main()
 {
 
